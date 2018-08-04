5c261dd93296152479eba824baf10f3626d3804d
Milko Yanakiev <myanakiev@hotmail.com>
2018-08-04 11:23:51 +0300

    initial

--- src/test/java/rpg_tests/DummyTests.java
+++ src/test/java/rpg_tests/DummyTests.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import rpg_lab.Axe;
 import rpg_lab.Dummy;
 
-public class AxeTests {
+public class DummyTests {
     private Dummy dummy;
     private Axe axe;
 
@@ -17,22 +17,26 @@ public class AxeTests {
     }
 
     @Test
-    public void weaponAttackLosesDurability(){
+    public void dummyLosesHealthIfAttacked() {
         axe.attack(dummy);
-        Assert.assertEquals(9, axe.getDurabilityPoints());
+        Assert.assertEquals(0, dummy.getHealth());
     }
 
     @Test(expected = IllegalStateException.class)
-    public void brokenWeaponCanAttack() {
+    public void deadDummyThrowsExceptionIfAttacked() {
         axe.attack(dummy);
         axe.attack(dummy);
     }
 
+    @Test
+    public void deadDummyCanGiveXP() {
+        axe.attack(dummy);
+        Assert.assertEquals(10, dummy.giveExperience());
+    }
 
-
-
-
-
-
+    @Test(expected = IllegalStateException.class)
+    public void aliveDummyCanNotGiveXP() {
+        dummy.giveExperience();
+    }
 
 }
